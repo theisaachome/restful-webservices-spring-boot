0cc967af80a4ce14dc4adb173e911c6fa4037d63
theisaachome <65864160+theisaachome@users.noreply.github.com>
2024-01-13 00:07:30 +0630

    added AppConstants files.

--- spring-blog-api-v3/src/main/java/com/isaachome/bookstore/controller/AuthorController.java
+++ spring-blog-api-v3/src/main/java/com/isaachome/bookstore/controller/AuthorController.java
@@ -3,6 +3,7 @@ package com.isaachome.bookstore.controller;
 import com.isaachome.bookstore.dto.AuthorDTO;
 import com.isaachome.bookstore.dto.AuthorResponse;
 import com.isaachome.bookstore.service.AuthorService;
+import com.isaachome.bookstore.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,10 +35,10 @@ public class AuthorController {
 //    }
     @GetMapping
     public AuthorResponse getAllAuthor(
-                                        @RequestParam(value = "pageNo" ,defaultValue = "0",required = false) int pageNo,
-                                        @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
-                                        @RequestParam(value = "sortBy",defaultValue = "id",required = false)String sortBy,
-                                        @RequestParam(value = "sortDir",defaultValue = "asc",required = false)String sortDir){
+                                        @RequestParam(value = "pageNo" ,defaultValue = AppConstants.DEFAULT_PAGE_NUMBER,required = false) int pageNo,
+                                        @RequestParam(value = "pageSize",defaultValue = AppConstants.DEFAULT_PAGE_SIZE,required = false) int pageSize,
+                                        @RequestParam(value = "sortBy",defaultValue = AppConstants.DEFAULT_SORT_BY_ID,required = false)String sortBy,
+                                        @RequestParam(value = "sortDir",defaultValue =AppConstants.DEFAULT_SORT_DIRECTION,required = false)String sortDir){
 //        return  authorService.getAllAuthor(pageNo,pageSize);
         return  authorService.getAllAuthor(pageNo,pageSize,sortBy,sortDir);
     }
